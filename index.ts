728896bbda870a77561d1ba18d2d4468ceafa81c
AsjadRehman <miasjad@gmail.com>
2024-03-24 14:41:17 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+console.log("Welcome to my calculator")
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {message:"Enter first number", type:"number", name:"firstNumber"},
+    {message:"Enter second number", type:"number", name:"secondNumber"},
+    {message:"Select one Arithmetic Operation:", 
+    type:"list",
+    name:"operator",
+    choices:["Addition","Subtraction","Multiplication", "Division"]}
+]);
+if(answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}  else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}  else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+} else ("Please select a valid operator")
+
